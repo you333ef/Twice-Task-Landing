6d1fca11ef116c6cecdb2a3f5f51e0e70b06b843
Yousef Eljoo <ghhjhkkoooppppplll@gmail.com>
2024-07-11 23:23:35 -0700

    وصف للتعديلات التي قمت بها

--- Java.js
+++ Java.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const options = {
+        duration: 10 // الثواني
+    };
+
+    const projectsCounter = new CountUp('projects-counter', 0, 400, 0, options.duration);
+    const clientsCounter = new CountUp('clients-counter', 0, 600, 0, options.duration);
+    const stylesCounter = new CountUp('styles-counter', 0, 100, 0, options.duration);
+
+    if (!projectsCounter.error) {
+        projectsCounter.start();
+    } else {
+        console.error(projectsCounter.error);
+    }
+
+    if (!clientsCounter.error) {
+        clientsCounter.start();
+    } else {
+        console.error(clientsCounter.error);
+    }
+
+    if (!stylesCounter.error) {
+        stylesCounter.start();
+    } else {
+        console.error(stylesCounter.error);
+    }
+});
+
+
 class ScrollButton {
     constructor(buttonId, showAt) {
         this.button = document.getElementById(buttonId);
@@ -27,7 +56,7 @@ class ScrollButton {
     }
 }
 
-// تحديد نقطة الظهور (بالبكسل)
+
 const showAt = 100; // يظهر بعد 100 بكسل من الهبوط
 
 // إنشاء كائن من الكلاس ScrollButton
